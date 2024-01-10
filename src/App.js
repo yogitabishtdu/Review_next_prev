a31dca69346ac7054170395aa37c0b4a06a1e48d
Yogita <yogitabishtdu@gmail.com>
2024-01-10 19:15:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Data from "./components/Data";
+import { useState } from "react";
 function App() {
+  const [review2, setReview2] = useState(Data);
+  const [reviewIndex, setReviewIndex] = useState(0);
+
+  function handlePrevious(id) {
+    setReviewIndex((id) => id - 1);
+  }
+
+  function handleNext(id) {
+    setReviewIndex((id) => id + 1);
+  }
+
+  function handleSurprise(id) {
+    let randomNumber = Math.floor(Math.random() * review2.length);
+
+    if (randomNumber === id) {
+      randomNumber = id + 1;
+    }
+    console.log(randomNumber);
+    setReviewIndex(randomNumber);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Our Reviews</h1>
+      <p>Total number of reviews: {review2.length}</p>
+      <hr />
+      <br />
+      <div>
+        <img src={review2[reviewIndex].image} alt={review2[reviewIndex].name} />
+        <h2>{review2[reviewIndex].name}</h2>
+        <p>Reviewer id: {review2[reviewIndex].id - 1}</p>
+        <h3 style={{ color: "blue" }}>{review2[reviewIndex].job}</h3>
+        <p>{review2[reviewIndex].text}</p>
+      </div>
+      <div>
+        <button
+          disabled={reviewIndex === 0}
+          onClick={() => handlePrevious(reviewIndex)}
         >
-          Learn React
-        </a>
-      </header>
+          previous
+        </button>
+        <button
+          disabled={reviewIndex === review2.length - 1}
+          onClick={() => handleNext(reviewIndex)}
+        >
+          next
+        </button>
+      </div>
+      <br />
+      <button onClick={() => handleSurprise(reviewIndex)}>Surprise Me</button>
     </div>
   );
 }
